959d3ce8aa5a8374d58bd7dbaafb28221297f666
Harminder Virk <virk.officials@gmail.com>
2017-08-01 12:40:32 +0530

    feat(instructions): add instructions file for ace

---
+++ instructions.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*
+ * adonis-session
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+const path = require('path')
+
+module.exports = async function (cli) {
+  await cli.makeConfig('session.js', path.join(__dirname, './templates/config.mustache'))
+}
